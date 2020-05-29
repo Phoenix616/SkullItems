ea40223bf2bb6886fea375f00949de89663922d7
Phoenix616 <mail@moep.tv>
2020-05-29 20:55:30 +0100

    Update pom, version and relicense to GPLv3

--- src/main/java/de/themoep/specialitems/SpecialItems.java
+++ src/main/java/de/themoep/specialitems/SpecialItems.java
@@ -1,27 +1,29 @@
 package de.themoep.specialitems;
 
+/*
+ * SpecialItems
+ * Copyright (c) 2020 Max Lee aka Phoenix616 (mail@moep.tv)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 import de.themoep.specialitems.listeners.ActionTriggerListener;
 import de.themoep.specialitems.listeners.ItemCraftListener;
 import org.bukkit.ChatColor;
 import org.bukkit.NamespacedKey;
 import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
-
-/**
- * Copyright 2016 Max Lee (https://github.com/Phoenix616/)
- * <p/>
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the Mozilla Public License as published by
- * the Mozilla Foundation, version 2.
- * <p/>
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * Mozilla Public License v2.0 for more details.
- * <p/>
- * You should have received a copy of the Mozilla Public License v2.0
- * along with this program. If not, see <http://mozilla.org/MPL/2.0/>.
- */
 
 public class SpecialItems extends JavaPlugin {
     public static NamespacedKey KEY;
--- src/main/java/de/themoep/specialitems/actions/TargetedTrigger.java
+++ src/main/java/de/themoep/specialitems/actions/TargetedTrigger.java
@@ -1,5 +1,23 @@
 package de.themoep.specialitems.actions;
 
+/*
+ * SpecialItems
+ * Copyright (c) 2020 Max Lee aka Phoenix616 (mail@moep.tv)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 import de.themoep.specialitems.SpecialItem;
 import org.bukkit.Location;
 import org.bukkit.block.Block;
@@ -10,21 +28,6 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.Event;
 import org.bukkit.inventory.ItemStack;
 
-/**
- * Copyright 2016 Max Lee (https://github.com/Phoenix616/)
- * <p/>
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the Mozilla Public License as published by
- * the Mozilla Foundation, version 2.
- * <p/>
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * Mozilla Public License v2.0 for more details.
- * <p/>
- * You should have received a copy of the Mozilla Public License v2.0
- * along with this program. If not, see <http://mozilla.org/MPL/2.0/>.
- */
 public class TargetedTrigger extends Trigger {
     private TriggerTarget target;
 
